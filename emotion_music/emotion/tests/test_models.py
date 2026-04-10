1f941076bf0e1b46b9876e54505d05be9692d73b
emmyfreshh <emma1top@gmail.com>
2026-04-10 01:50:56 +0100

    removed test files

--- emotion_music/emotion/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from emotion_music.emotion.models import Emotion  # Replace with your actual model
-
-class EmotionModelTest(TestCase):
-    def test_create_emotion(self):
-        emotion = Emotion.objects.create(name="happy")
-        self.assertEqual(str(emotion), "happy")
