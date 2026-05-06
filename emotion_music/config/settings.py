dba7b94b06cca981e1b77e315e54efe25bac63ab
emmyfreshh <emma1top@gmail.com>
2026-05-06 21:05:25 +0100

    update settings and url

--- emotion_music/config/settings.py
+++ emotion_music/config/settings.py
@@ -130,8 +130,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static' ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    
 
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
